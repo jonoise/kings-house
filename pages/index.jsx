c19bbe428e2afa2a6b0a92f6c36286a08ad61f45
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-12 18:58:28 -0600

    add landing page

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Divider, Flex, Text, VStack } from '@chakra-ui/react'
+import Lottie from 'lottie-react'
+import lottieJson from '../6519-cooking.json'
+import { colors } from '../globals'
+export default function Home() {
+  return (
+    <VStack
+      w="full"
+      minH="100vh"
+      justify="center"
+      align="center"
+      color={colors.main}
+      textAlign="center"
+    >
+      <Flex
+        minW={['240px']}
+        maxW="300px"
+        minH={['60px']}
+        bg="white"
+        position="relative"
+      >
+        <Image src="/images/kings_house_lettering.png" layout="fill" alt="" />
+      </Flex>
+      <Lottie animationData={lottieJson} autoplay={true} loop={true} />
+      <Text fontWeight="bold">ESTAMOS COCINANDO EL PLATO PRINCIPAL.</Text>
+      <Divider />
+      <Text fontSize="14px" fontWeight="bold">
+        PRONTO EN SAN RAMÓN.
+      </Text>
+    </VStack>
+  )
+}
