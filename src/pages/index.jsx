613756af9c5e5d88e4c088b556d0e21bf738aec5
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-17 22:23:55 -0600

    add head to index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,6 +17,26 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <title>{"THE BEST F$%&#ING SANDWICH IN TOWN! 💣| King's House"}</title>
+        <meta
+          name="description"
+          content="Cuidamos cada detalle, cada vegetal, caga gramo de harina en el pan para que nuestros clientes degusten un producto de calidad."
+        ></meta>
+        <meta name="keywords" content="Sandwich, San Ramón, Kings House"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:title"
+          content="THE BEST F$%&#ING SANDWICH IN TOWN! 💣"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Cuidamos cada detalle, cada vegetal, caga gramo de harina en el pan para que nuestros clientes degusten un producto de calidad."
+        ></meta>
+        <meta property="og:url" content="https://kingshousecr.com" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/71573508/137668838-c8539b28-aa0a-4f97-98a9-5fdc27baed1c.png"
+        ></meta>
       </Head>
       <AnimatePresence exitBeforeEnter>
         {loading ? (
