7fece447d01e8721fcf21a45fc3eac392a84a6a2
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-19 20:54:41 -0600

    add colors

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,11 @@
 export const colors = {
     main: "#482ff7",
-    secondary: '#ff3381',
-    white: 'whitesmoke'
+    mainLight: "#2d6cdf",
+    mainLighter: "#46c3db",
+    secondary: '#ff304f',
+    yellow: '#f3f169',
+    beige: "96A7AB",
+    paleDark: "685E79",
+    dark: '#171722',
+    white: '#EDEEF0'
 }
