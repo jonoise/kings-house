656d460067a00941a6594fefc202257e5a44cc28
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-19 20:53:36 -0600

    add menu rout

---
+++ src/pages/menu.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { BrowserView, MobileOnlyView } from 'react-device-detect'
+import DesktopMenu from '../components/desktop/menu'
+import HomeHead from '../components/heads/HomeHead'
+import LoadingScreen from '../components/loadingScreen'
+import axios from '../lib/http'
+import useProductsStore from '../stores/useProductsStore'
+
+const Menu = ({ categories }) => {
+  const [loading, setLoading] = useState(true)
+  const setCategories = useProductsStore((state) => state.setCategories)
+  const setCategoriesHash = useProductsStore((state) => state.setCategoriesHash)
+
+  useEffect(() => {
+    setCategories(categories)
+    setCategoriesHash(categories)
+    setLoading(false)
+  }, [categories, setCategories, setCategoriesHash])
+
+  return (
+    <div>
+      <HomeHead />
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <BrowserView>
+            <DesktopMenu />
+          </BrowserView>
+          <MobileOnlyView></MobileOnlyView>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Menu
+
+export const getStaticProps = async () => {
+  const res = await axios.get('products/categories')
+  const categories = await res.data
+
+  return {
+    props: {
+      categories,
+    },
+  }
+}
