6eb65fbee0632bf39bacfc4ee5d5a3c7a171b09d
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-19 20:54:30 -0600

    add keys

--- src/stores/useProductsStore.jsx
+++ src/stores/useProductsStore.jsx
@@ -1,18 +1,37 @@
 import create from 'zustand'
+import { devtools } from 'zustand/middleware'
 
-const useProductsStore = create((set) => ({
-  products: null,
-  currentProduct: null,
-  setProducts: (products) =>
-    set((state) => ({
-      ...state,
-      products: products,
-    })),
-  setCurrentProduct: (product) =>
-    set((state) => ({
-      ...state,
-      currentProduct: product,
-    })),
-}))
+const useProductsStore = create(
+  devtools((set) => ({
+    categories: null,
+    categoriesHash: null,
+    products: null,
+    currentCategory: 'subs',
+    currentImage: null,
+    setCategories: (categories) =>
+      set((state) => ({
+        ...state,
+        categories: categories,
+      })),
+    setCurrentCategory: (category) =>
+      set((state) => ({
+        ...state,
+        currentCategory: category,
+      })),
+    setCurrentImage: (image) =>
+      set((state) => ({
+        ...state,
+        currentImage: `http://localhost:8000${image}`,
+      })),
+    setCategoriesHash: (categories) =>
+      set((state) => {
+        let categoriesHash = {}
+        categories.map((cat) => (categoriesHash[cat.slug] = cat))
+        return {
+          categoriesHash: categoriesHash,
+        }
+      }),
+  }))
+)
 
 export default useProductsStore
