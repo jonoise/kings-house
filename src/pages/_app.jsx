07c9259ed1651a0d1994adc82fa4bbccfcf8aeb9
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-24 01:46:54 -0600

    add swiper

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 import '../styles/globals.css'
 import Fonts from '../components/fonts'
+import 'swiper/css/bundle'
 
 function MyApp({ Component, pageProps }) {
   return (
