dd41fe7b672d5cf671a3febd401174b8a8307505
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-24 18:35:06 -0600

    edit footer

--- src/components/menu/drawers/ProductDrawer.jsx
+++ src/components/menu/drawers/ProductDrawer.jsx
@@ -91,12 +91,14 @@ const ProductDrawer = () => {
                     setQuantity={setQuantity}
                   />
                 </Stack>
-                <AddToCart
-                  description={description}
-                  setDescription={setDescription}
-                  quantity={quantity}
-                  setQuantity={setQuantity}
-                />
+                <Flex w="full" pb="4rem">
+                  <AddToCart
+                    description={description}
+                    setDescription={setDescription}
+                    quantity={quantity}
+                    setQuantity={setQuantity}
+                  />
+                </Flex>
               </Stack>
             </DrawerBody>
           </DrawerContent>
